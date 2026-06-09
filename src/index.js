0a184f412b42afb50a0f68057845ecec2062a920
Teatoes <benwang1515@gmail.com>
2026-06-09 17:23:07 -0500

    fixed refresh bug

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+const redirect = sessionStorage.redirect;
+
+if (redirect) {
+  delete sessionStorage.redirect;
+  window.history.replaceState(null, null, redirect);
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
